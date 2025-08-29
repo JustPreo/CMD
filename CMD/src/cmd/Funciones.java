46c4dac3c0caa9434cb2ea83dba3e3e520f65bff
JustPreo <impre0hn@gmail.com>
2025-08-29 08:51:02 -0600

    a

---
+++ CMD/src/cmd/Funciones.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cmd;
+
+import java.io.File;
+
+/**
+ *
+ * @author user
+ */
+public class Funciones {
+    private File file = null;
+    
+    public void setFile(String dir)
+    {
+        file = new File(dir);
+    
+    
+    }
+    
+}
